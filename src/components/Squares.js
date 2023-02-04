a9fdb651cbdd877eb727736068965ca3630d6fdf
radhika <radhika_rb@hotmail.com>
2023-02-04 21:12:10 +0530

    in circle

--- src/components/Squares.js
+++ src/components/Squares.js
@@ -3,29 +3,32 @@ import React from 'react';
 import InputBox from './InputBox';
 import Square from './Square';
 
-const movePos = 50;
+const halfWidth = 0;
 const exponent = 2;
 const margin = 2;
+const radius = 100;
+
+const two = 2;
+// const colNo = index % number;
+// const rowNo = Math.floor(index / number);
 
 const Squares = (context) => {
 	const { state: { number }} = context;
 
 	const totalSquares = Math.pow(number, exponent);
+	const theta = two * Math.PI / number;
 
 	return <div className="container">
 		<InputBox { ...context }/>
 		<div className="outerBox">
-			{range(0, totalSquares).map((dummy, index) => {
-				const colNo = index % number;
-				const rowNo = Math.floor(index / number);
-
-				return (
-					<Square
-						key={ index }
-						movePosY={ (movePos + margin) * rowNo }
-						movePosX={ (movePos + margin)	* colNo }
-					/>);
-			})}
+			{range(0, number).map((dummy, index) =>
+				<Square
+					key={ index }
+					movePosY={ halfWidth
+						+ radius * Math.sin(theta * index) + radius }
+					movePosX={ halfWidth
+						+ radius * Math.cos(theta * index) + radius }
+				/>)}
 		</div>
 	</div>;
 };
