e4c454d6191581733a1499b663e51661c19c18bb
radhika <radhika_rb@hotmail.com>
2023-02-04 17:04:38 +0530

    single loop

--- src/components/Square.js
+++ src/components/Square.js
@@ -1,6 +1,15 @@
 import React from 'react';
 
-const Square = ({ key }) =>
-	<div key={ key } className="innerBox"/>;
+const Square = ({ key, movePosX, movePosY }) =>
+	<div
+		key={ key }
+		style={ {
+			position: 'absolute',
+			left: `${ movePosX }px`,
+			top: `${ movePosY }px`,
+		} }
+		className="innerBox "
+	/>
+	;
 
 export default Square;
--- src/components/Squares.js
+++ src/components/Squares.js
@@ -3,18 +3,29 @@ import React from 'react';
 import InputBox from './InputBox';
 import Square from './Square';
 
+const movePos = 50;
+const exponent = 2;
+const margin = 2;
+
 const Squares = (context) => {
 	const { state: { number }} = context;
+
+	const totalSquares = Math.pow(number, exponent);
 
 	return <div className="container">
 		<InputBox { ...context }/>
 		<div className="outerBox">
-			{	range(0, number).map((dummy, indexRow) =>
-				<div key={ indexRow } className="displayBlock">
-					{range(0, number).map((dumm, indexCol) =>
-						<Square key={ indexCol }/>)}
+			{range(0, totalSquares).map((dummy, index) => {
+				const colNo = index % number;
+				const rowNo = Math.floor(index / number);
 
-				</div>)}
+				return (
+					<Square
+						key={ index }
+						movePosY={ (movePos + margin) * rowNo }
+						movePosX={ (movePos + margin)	* colNo }
+					/>);
+			})}
 		</div>
 	</div>;
 };
